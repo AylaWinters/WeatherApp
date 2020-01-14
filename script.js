2f4632a0f163b0eb3e6c54cb1e5b9a0393ea4340
Andre Entrekin <andre.entrekin@gmail.com>
2020-01-13 18:32:47 -0600

    new features

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 var d = new Date();
 var today = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear()
 
-var day1 = (d.getMonth() + 1) + "/" + (d.getDate() + 1) + "/" + d.getFullYear()
-$("#day1").text(day1)
+// var day1 = (d.getMonth() + 1) + "/" + (d.getDate() + 1) + "/" + d.getFullYear()
+// $("#day1").text(day1)
 
-var day2 = (d.getMonth() + 1) + "/" + (d.getDate() + 2) + "/" + d.getFullYear()
-$("#day2").text(day2);
+// var day2 = (d.getMonth() + 1) + "/" + (d.getDate() + 2) + "/" + d.getFullYear()
+// $("#day2").text(day2);
 
-var day3 = (d.getMonth() + 1) + "/" + (d.getDate() + 3) + "/" + d.getFullYear()
-$("#day3").text(day3);
+// var day3 = (d.getMonth() + 1) + "/" + (d.getDate() + 3) + "/" + d.getFullYear()
+// $("#day3").text(day3);
 
-var day4 = (d.getMonth() + 1) + "/" + (d.getDate() + 4) + "/" + d.getFullYear()
-$("#day4").text(day4);
+// var day4 = (d.getMonth() + 1) + "/" + (d.getDate() + 4) + "/" + d.getFullYear()
+// $("#day4").text(day4);
 
-var day5 = (d.getMonth() + 1) + "/" + (d.getDate() + 5) + "/" + d.getFullYear()
-$("#day5").text(day5);
+// var day5 = (d.getMonth() + 1) + "/" + (d.getDate() + 5) + "/" + d.getFullYear()
+// $("#day5").text(day5);
 
 
 $(document).ready(function () {
@@ -24,78 +24,91 @@ $(document).ready(function () {
         // event.preventDefault();
         // location.reload()
 
+
         var city = $("#search").val();
+        if (city === "") {
+            $("#err").text("Please enter a city!")
+        } else {
 
-        queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=88ec15a371a789a350f0798f4a6bd272"
-
-        // ajax to pull data
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-
-
-            //fill in data to display
-            var iconCode = response.weather[0].icon
-            var iconURL = ("http://openweathermap.org/img/wn/" + iconCode + "@2x.png")
-
-            $("#name").text(response.name + " " + today)
-            $(".icon").attr('src', iconURL)
-            $("#temp").text(((response.main.temp - 273.15) * 9 / 5 + 32).toFixed(0))
-            $("#hum").text(response.main.humidity)
-            $("#wind").text(response.wind.speed)
-
-        });
-
-        // fill in data to cards
-        query2URL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=88ec15a371a789a350f0798f4a6bd272"
-
-        // ajax to pull 5 day data
-        $.ajax({
-            url: query2URL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-
-
-            $("#temp1").text("Temp: " + ((response.list[5].main.temp - 273.15) * 9 / 5 + 32).toFixed(0))
-            $("#hum1").text("Humidity: " + response.list[5].main.humidity)
-            var ic1 = response.list[5].weather[0].icon
-            var iURL1 = ("http://openweathermap.org/img/wn/" + ic1 + "@2x.png")
-            $(".icon1").attr("src", iURL1)
+            $(".display").removeClass("hide");
+            $(".five").removeClass("hide");
 
 
 
-            $("#temp2").text("Temp: " + ((response.list[13].main.temp - 273.15) * 9 / 5 + 32).toFixed(0))
-            $("#hum2").text("Humidity: " + response.list[13].main.humidity)
-            var ic2 = response.list[13].weather[0].icon
-            var iURL2 = ("http://openweathermap.org/img/wn/" + ic2 + "@2x.png")
-            $(".icon2").attr("src", iURL2)
+
+            queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=88ec15a371a789a350f0798f4a6bd272"
 
 
-            $("#temp3").text("Temp: " + ((response.list[21].main.temp - 273.15) * 9 / 5 + 32).toFixed(0))
-            $("#hum3").text("Humidity: " + response.list[21].main.humidity)
-            var ic3 = response.list[21].weather[0].icon
-            var iURL3 = ("http://openweathermap.org/img/wn/" + ic3 + "@2x.png")
-            $(".icon3").attr("src", iURL3)
+            // ajax to pull data
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+
+                console.log(response);
 
 
-            $("#temp4").text("Temp: " + ((response.list[29].main.temp - 273.15) * 9 / 5 + 32).toFixed(0))
-            $("#hum4").text("Humidity: " + response.list[29].main.humidity)
-            var ic4 = response.list[29].weather[0].icon
-            var iURL4 = ("http://openweathermap.org/img/wn/" + ic4 + "@2x.png")
-            $(".icon4").attr("src", iURL4)
+                //fill in data to display
+                var iconCode = response.weather[0].icon
+                var iconURL = ("http://openweathermap.org/img/wn/" + iconCode + "@2x.png")
+
+                $("#name").text(response.name + " " + today)
+                $(".icon").attr('src', iconURL)
+                $("#temp").text(((response.main.temp - 273.15) * 9 / 5 + 32).toFixed(0))
+                $("#hum").text(response.main.humidity)
+                $("#wind").text(response.wind.speed)
+
+            });
+
+            // fill in data to cards
+            query2URL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=88ec15a371a789a350f0798f4a6bd272"
+
+            // ajax to pull 5 day data
+            $.ajax({
+                url: query2URL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
 
 
-            $("#temp5").text("Temp: " + ((response.list[37].main.temp - 273.15) * 9 / 5 + 32).toFixed(0))
-            $("#hum5").text("Humidity: " + response.list[37].main.humidity)
-            var ic5 = response.list[37].weather[0].icon
-            var iURL5 = ("http://openweathermap.org/img/wn/" + ic5 + "@2x.png")
-            $(".icon5").attr("src", iURL5)
+                $("#temp1").text("Temp: " + ((response.list[5].main.temp - 273.15) * 9 / 5 + 32).toFixed(0))
+                $("#hum1").text("Humidity: " + response.list[5].main.humidity)
+                var ic1 = response.list[5].weather[0].icon
+                var iURL1 = ("http://openweathermap.org/img/wn/" + ic1 + "@2x.png")
+                $(".icon1").attr("src", iURL1)
 
 
-        });
 
+                $("#temp2").text("Temp: " + ((response.list[13].main.temp - 273.15) * 9 / 5 + 32).toFixed(0))
+                $("#hum2").text("Humidity: " + response.list[13].main.humidity)
+                var ic2 = response.list[13].weather[0].icon
+                var iURL2 = ("http://openweathermap.org/img/wn/" + ic2 + "@2x.png")
+                $(".icon2").attr("src", iURL2)
+
+
+                $("#temp3").text("Temp: " + ((response.list[21].main.temp - 273.15) * 9 / 5 + 32).toFixed(0))
+                $("#hum3").text("Humidity: " + response.list[21].main.humidity)
+                var ic3 = response.list[21].weather[0].icon
+                var iURL3 = ("http://openweathermap.org/img/wn/" + ic3 + "@2x.png")
+                $(".icon3").attr("src", iURL3)
+
+
+                $("#temp4").text("Temp: " + ((response.list[29].main.temp - 273.15) * 9 / 5 + 32).toFixed(0))
+                $("#hum4").text("Humidity: " + response.list[29].main.humidity)
+                var ic4 = response.list[29].weather[0].icon
+                var iURL4 = ("http://openweathermap.org/img/wn/" + ic4 + "@2x.png")
+                $(".icon4").attr("src", iURL4)
+
+
+                $("#temp5").text("Temp: " + ((response.list[37].main.temp - 273.15) * 9 / 5 + 32).toFixed(0))
+                $("#hum5").text("Humidity: " + response.list[37].main.humidity)
+                var ic5 = response.list[37].weather[0].icon
+                var iURL5 = ("http://openweathermap.org/img/wn/" + ic5 + "@2x.png")
+                $(".icon5").attr("src", iURL5)
+
+
+            });
+        }
 
         var list = $("#list");
         var cityArr
@@ -109,10 +122,20 @@ $(document).ready(function () {
             // assign value from lS to cityArr
             cityArr = []
         }
+        // city.charAt(0).toUpperCase()
+
+        // str.replace("Microsoft", "W3Schools");
+        var upperCase = city.replace(city.charAt(0), city.charAt(0).toUpperCase())
 
 
+        // for (let i = 0; i < cityArr.length; i++) {
+        //     if (city);
 
-        cityArr.push(city)
+        // }
+
+        if (city != "") {
+            cityArr.push(upperCase)
+        }
 
         storeCities()
         //input to local storage
@@ -125,10 +148,13 @@ $(document).ready(function () {
         // pull from local storage
 
         var li = $("<li>").addClass("buttons")
-        li.text(city)
-        list.append(li)
+        li.text(upperCase)
+        if (city != "") {
+            list.append(li)
 
+        }
 
+        // window.location.reload();
     });
 
 
@@ -157,6 +183,8 @@ $(document).ready(function () {
     $(document).on("click", ".buttons", function () {
         console.log($(this).html())
         var cityName = $(this).html()
+        $(".display").removeClass("hide");
+        $(".five").removeClass("hide");
 
         queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=88ec15a371a789a350f0798f4a6bd272"
 
@@ -193,6 +221,32 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             console.log(response);
+
+            // start at index 4
+            //loop 5 times
+            let day = 1;
+            for (let i = 4; i < response.list.length; i = i + 8) {
+                // grab that date
+                var dt_time = response.list[i].dt_txt;
+                var mo_day = dt_time.substring(5, 10)
+
+                if (mo_day.charAt(0) === 0) {
+                    console.log("hello")
+                }
+
+                year = dt_time.substring(2, 4)
+                console.log();
+
+                dt_time = mo_day.replace('-', '/') + '/' + year;
+                console.log(dt_time);
+                $('#day' + day).text(dt_time);
+                day += 1;
+
+
+            }
+
+
+
 
 
             $("#temp1").text("Temp: " + ((response.list[5].main.temp - 273.15) * 9 / 5 + 32).toFixed(0))
